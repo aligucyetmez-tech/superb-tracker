c84b28d4faa5f1aa33bcc0e78941f9fd1f1049d0
aligucyetmez-tech <aligucyetmez@icloud.com>
2026-07-26 05:01:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+SEARCH_URLS = [
+    "https://www.sahibinden.com/arama?a116445=1263354&a6=32466&a4_max=40000&sorting=price_asc&a5_min=2025&category=254356&category=257420&category=256774&unchangingTracks=true&utm_source=paylas&utm_medium=arama_sonuc&utm_campaign=sahibinden_paylas&utm_content=174536269",
+    "https://www.sahibinden.com/bmw-3-serisi-320i?unpaintedParts=true&a116445=1263354&utm_campaign=sahibinden_paylas&utm_medium=arama_sonuc&a4_max=50000&sorting=price_asc&a5_min=2024&unchangingTracks=true&utm_content=174536269&utm_source=paylas",
+]
+
+CHECK_INTERVAL = 120
