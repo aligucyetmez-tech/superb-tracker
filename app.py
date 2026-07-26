9ee0b4053c3ed8c4919e3e88606f139e68fdb478
aligucyetmez-tech <aligucyetmez@icloud.com>
2026-07-26 05:05:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Superb Tracker Başlatıldı")
+from tracker import get_listings
+
+if __name__ == "__main__":
+    listings = get_listings()
+
+    print(f"{len(listings)} ilan bulundu.")
+
+    for listing in listings:
+        print(listing)
